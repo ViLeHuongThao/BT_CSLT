28ab81b2275e3470d5eb321f1ee70dd441b558a2
ViLeHuongThao <vile.31231026488@st.ueh.edu.vn>
2024-11-24 18:50:43 +0700

    Add files via upload

---
+++ Bài tập Cơ sở lập trình/SS06.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_tập_Cơ_sở_lập_trình
+{
+
+    internal class SS06
+    {
+        static void Main()
+        {
+            //EX01();
+            EX02();
+            Console.ReadKey();
+        }
+        static void EX01()
+        {
+            Console.Write("Enter a number: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter a number: ");
+                bool res = int.TryParse(Console.ReadLine(), out array[i]);
+                if (!res)
+                {
+                    Console.Write(" Please enter a number: ");
+                    i--;
+                }
+            }
+            foreach (int item in array) { Console.WriteLine(item); }
+            Console.WriteLine("new array: ");
+            //cộng mỗi phần tử thêm 2
+            change(array);
+            //foreach (int item in array) { Console.WriteLine(item); }
+            for (int count = 0; count < array.Length; count++) { Console.WriteLine(array[count]); }//in mảng mới
+
+            static void change(int[] array) //function
+            {
+                for (int i = 0; i < array.Length; i++)
+                    array[i] += 2;
+
+            }
+        }
+
+
+        static void EX02()
+        {
+            //Tạo 1 mảng ngẫu nhiên
+            Console.Write("Enter array length: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+            CreateARandomArray(array, length);
+            void CreateARandomArray(int[] array, int length)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < length; i++) { array[i] = rnd.Next(0, 100); }
+
+                //linear search:
+                static int LinearSearch(int[] array, int x)
+                {
+                    for (int index = 0; index < array.Length; index++)
+                        if (array[index] == x)
+                            return index;
+                    return -1;
+                }
+            }
+        }
+    }
+}
+
---
+++ Bài tập Cơ sở lập trình/SS07.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_tập_Cơ_sở_lập_trình
+{
+
+    internal class SS07
+    {
+        static void Main()
+            {
+                EX01();
+            Console.ReadKey();
+            }
+
+            static void TaoMang(int[,] a, int rows, int columns)
+            {
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        Random rnd = new Random();
+                        a[i, j] = rnd.Next(0, 100);
+                    }
+                }
+            }
+            static void XuatMang(int[,] a)
+            {
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        Console.Write(a[i, j] + "\t");
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+            static void InCot(int[,] a, int y)
+            {
+                if (y >= a.GetLength(1) || y < 0)
+                {
+                    Console.WriteLine($"Khong co cot thu {y}");
+                    return;
+                }
+                Console.WriteLine($"Cot thu {y}: ");
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    Console.WriteLine(a[i, y]);
+                }
+
+            }
+            static void InDong(int[,] a, int y)
+            {
+                if (y >= a.GetLength(0) || y < 0)
+                {
+                    Console.WriteLine($"Khong co dong thu {y}");
+                    return;
+                }
+                Console.Write($"Dong thu {y}: ");
+                for (int i = 0; i < a.GetLength(1); i++)
+                {
+                    Console.Write(a[y, i] + "\t");
+                }
+            }
+
+            static int TimMax(int[,] a, ref int max)
+            {
+
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if (a[i, j] > max) { max = a[i, j]; }
+                    }
+                }
+                return max;
+            }
+            static int TimMin(int[,] a, ref int min)
+            {
+
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if (a[i, j] < min) { min = a[i, j]; }
+                    }
+                }
+                return min;
+            }
+            static void EX01()
+            {
+                int[,] a;
+                Console.Write("Nhap so dong: ");
+                int rows = int.Parse(Console.ReadLine());
+                Console.Write("Nhap so cot: ");
+                int cols = int.Parse(Console.ReadLine());
+                a = new int[rows, cols];
+                TaoMang(a, rows, cols);
+                XuatMang(a);
+                Console.WriteLine();
+
+                Console.Write("Ban muon in dong hay cot: "); string n; n = Console.ReadLine().ToLower();
+                Console.Write("Ban muon in dong/cot thu may: "); int y = int.Parse(Console.ReadLine());
+                if (n == "cot") { InCot(a, y); };
+                if (n == "dong") { InDong(a, y); };
+
+                int max = int.MinValue;
+                TimMax(a, ref max);
+                Console.WriteLine("So lon nhat trong mang la: " + max);
+
+                int min = int.MaxValue;
+                TimMin(a, ref min);
+                Console.WriteLine("So nho nhat trong mang la: " + min);
+            }
+        }
+    }
+
+
+
+
